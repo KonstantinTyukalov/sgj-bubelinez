cf3eb97f1c609db9fb2bd51cbd4d4841d7c30c3a
JovannyM <JovannyM@mail.ru>
2021-11-05 02:35:50 +0300

    Added solution. Added .gitignore (#1)
    * Added solution. Added .gitignore
    
    * Added sfml onto dependencies
    
    Co-authored-by: Denis Rumyantsev <61472718+DenisRumyantsev@users.noreply.github.com>
    
    Co-authored-by: Konstantin Tyukalov <52399739+QinamruG@users.noreply.github.com>
    Co-authored-by: Denis Rumyantsev <61472718+DenisRumyantsev@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bubelinez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
