71e74ff60393971eacdc19c4a794ba1206bf1ff9
JovannyM <jovannym@mail.ru>
2021-11-05 17:43:38 +0300

    Added constants

--- RenameIt/MainWindow.cs
+++ RenameIt/MainWindow.cs
@@ -1,33 +1,44 @@
-﻿using SFML.Graphics;
+﻿using System;
+using SFML.Graphics;
 using SFML.Window;
 
 namespace Bubelinez.RenameIt
 {
     public class MainWindow
     {
-        private RenderWindow window;
+        private readonly RenderWindow _window;
+        private const uint _windowWidth = 1280;
+        private const uint _windowHeight = 720;
+        private const string _windowName = "BUBELINEZ";
 
         public MainWindow()
         {
-            window = new RenderWindow(new VideoMode(1280, 720), "DigitalBattle", Styles.Close);
-            window.SetFramerateLimit(60);
-            window.SetVerticalSyncEnabled(true);
-            window.SetKeyRepeatEnabled(false);
+            VideoMode videoMode = new VideoMode(_windowWidth, _windowHeight);
+            ContextSettings contextSettings = new ContextSettings(0, 0, 1);
+            _window = new RenderWindow(new VideoMode(_windowWidth, _windowHeight), _windowName, Styles.Fullscreen, contextSettings);
+            _window.SetFramerateLimit(60);
+            _window.SetVerticalSyncEnabled(true);
+            _window.SetKeyRepeatEnabled(false);
 
-            window.Closed += (sender, args) => { window.Close(); };
+            _window.Closed += WindowOnClosed;
         }
 
         public void Run()
         {
-            while (window.IsOpen)
+            while (_window.IsOpen)
             {
-                window.DispatchEvents();
-                window.Clear();
+                _window.DispatchEvents();
+                _window.Clear();
 
                 // Paste game logics this
                     
-                window.Display();
+                _window.Display();
             }
+        }
+
+        private void WindowOnClosed(object sender, EventArgs e)
+        {
+            _window.Close();
         }
     }
 }
