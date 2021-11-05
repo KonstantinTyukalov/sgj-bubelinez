e3f9cb12c10a13d32d1d64156de1fc0600d48c42
JovannyM <jovannym@mail.ru>
2021-11-05 17:51:29 +0300

    Added variables for context setting

--- Window/MainWindow.cs
+++ Window/MainWindow.cs
@@ -10,11 +10,14 @@ namespace Bubelinez.Window
         private const uint _windowWidth = 1280;
         private const uint _windowHeight = 720;
         private const string _windowName = "BUBELINEZ";
+        private const uint _depthBits = 0;
+        private const uint _stencilBits = 0;
+        private const uint antialiasingLevel = 1;
 
         public MainWindow()
         {
             VideoMode videoMode = new VideoMode(_windowWidth, _windowHeight);
-            ContextSettings contextSettings = new ContextSettings(0, 0, 1);
+            ContextSettings contextSettings = new ContextSettings(_depthBits, _stencilBits, antialiasingLevel);
             _window = new RenderWindow(new VideoMode(_windowWidth, _windowHeight), _windowName, Styles.Fullscreen, contextSettings);
             _window.SetFramerateLimit(60);
             _window.SetVerticalSyncEnabled(true);
