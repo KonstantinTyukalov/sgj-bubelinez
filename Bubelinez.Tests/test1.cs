81fafa2c54bd87c046f0467fb5a16fa754dd783c
QinamruG <qinamrug@gmail.com>
2021-11-06 16:51:45 +0300

    add sample test

---
+++ Bubelinez.Tests/test1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RPG.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestReturnToBasic()
+        {
+            Assert.Equal(true, true);
+        }
+    }
+}
